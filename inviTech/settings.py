0bf7922e6a58e774425048994f47ad61c13d9c2c
Oguntade Tawheed <59873785+TWEEDOriginal@users.noreply.github.com>
2020-09-07 19:46:34 +0100

    Update settings.py

--- inviTech/settings.py
+++ inviTech/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sv2p8hfna#eo08$2hae+r$l)pkxdc#b)x-68l7*t-#(l@mw(!k'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
